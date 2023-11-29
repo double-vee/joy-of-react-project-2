5c0f9ee41483bdb6c28b31ef0d532c679e2a21dd
Weronika Kansy <weronika.kansy@gmail.com>
2023-11-29 02:20:48 +0100

    Add ARIA attributes inside ToastShelf and Toast components to improve screen reader announcements

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -9,7 +9,12 @@ function ToastShelf() {
   const { toasts } = useContext(ToastContext);
 
   return (
-    <ol className={styles.wrapper}>
+    <ol
+      className={styles.wrapper}
+      role="region"
+      aria-live="polite"
+      aria-label="Notification"
+    >
       {toasts.map((item) => {
         return (
           <li key={item.id} className={styles.toastWrapper}>
